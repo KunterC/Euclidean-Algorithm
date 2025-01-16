0ee3f29379ea070d6d5edd536ef25b34f77fb5cb
Kunter <3075044C@student.gla.ac.uk>
2025-01-16 12:27:31 +0000

    modified code. does not allow negative numbers

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,12 +1,14 @@
-input_number_1 = int(input("Enter a number: "))
-input_number_2 = int(input("Enter another number: "))
+input_number_1 = int(input("Enter a number: ")) #gets the first user input
+input_number_2 = int(input("Enter another number: ")) #gets the second user input
 
 def greatest_common_divisor(num_1, num_2):
+    if num_1 < 0 or num_2 < 0: 
+        return "Please enter positive numbers only." #if any number is negative it returns an error message
+    
     while num_2 != 0: #while the second number is not 0
     
         num_1, num_2 = num_2, num_1 % num_2 #continuously sets first number as second 
                                             #and second as the remainder of the two numbers
-    return num_1 #returns first number when second number is 0
+    return f"The GCD of the numbers is {num_1}" #returns the GCD with a message
 
-gcd = greatest_common_divisor(input_number_1, input_number_2) #assigns a variable for the gcd.
-print(f"The GCD of {input_number_1} and {input_number_2} is {gcd}.") #prints the result
+print(greatest_common_divisor(input_number_1, input_number_2)) #prints the function
