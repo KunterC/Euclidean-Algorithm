71bb29426023975b9af20164dd1ecfda3be0a660
Kunter <3075044C@student.gla.ac.uk>
2025-01-16 11:49:51 +0000

    added code comments

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,13 +1,13 @@
-num_1 = 210
+num_1 = 210 #sets 2 variables for 2 numbers
 num_2 = 45
 
 def gcd(num_1, num_2):
-    while num_2 != 0:
+    while num_2 != 0: #while the second number is not 0
         
-        rem = num_1 % num_2
-        num_1 = num_2
-        num_2 = rem
-
+        rem = num_1 % num_2 #gets the remainder of two numbers
+        num_1 = num_2 #changes the first number as the second number
+        num_2 = rem #changes the second number as the remainder
+        #repeats process to find the greatest common divisor
     return num_1
 
-print(gcd(num_1, num_2))
+print(gcd(num_1, num_2)) #prints the result
